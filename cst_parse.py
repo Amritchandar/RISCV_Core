b6b61e1626fe214591a058e73ac31462e371335c
chiehanchen <a.chen.efk0@gmail.com>
2024-03-09 14:57:45 -0600

    python for control store

--- cst_parse.py
+++ cst_parse.py
@@ -13,11 +13,10 @@ def expand_x_combinations(values):
 
 def generate_verilog_code(data):
     verilog_code = "module control_store(\n"
-    verilog_code += "    input [ADDRESS_WIDTH-1:0] address,\n"
-    verilog_code += "    output reg [CONTROL_WIDTH-1:0] control_signals\n);\n\n"
-    verilog_code += "parameter ADDRESS_WIDTH = 10; \n"
-    verilog_code += "parameter CONTROL_WIDTH = 16; \n\n"
-    verilog_code += "reg [CONTROL_WIDTH-1:0] rom[0:{}];\n\n".format(len(data) * 2 - 2)
+    verilog_code += "    input [11:0] address,\n"
+    verilog_code += "    output [18:0] control_signals\n);\n\n"
+    verilog_code += "reg [18:0] rom[0:{}];\n\n".format(8000)
+    verilog_code += "assign control_signals = rom[address];\n"
     verilog_code += "initial begin\n"
 
     for row in data[1:]:  
@@ -26,7 +25,7 @@ def generate_verilog_code(data):
         func3 = row[2]
         IR30 = row[3]
         IR25 = row[4]
-        addr = opcode_binary + func3 + IR30 + IR30
+        addr = opcode_binary + func3 + IR30 + IR25
         control_signals = row[5:]
         combinations = expand_x_combinations(control_signals)
         for comb in combinations:
@@ -38,11 +37,11 @@ def generate_verilog_code(data):
     verilog_code += "end\nendmodule\n"
     return verilog_code
 
-file_path = 'cst1.csv'
+file_path = 'cst.csv'
 data = read_csv_data(file_path)
 verilog_code = generate_verilog_code(data)
 
-output_file_path = 'control_store_verilog.txt'
+output_file_path = 'control_store_verilog.v'
 with open(output_file_path, 'w') as output_file:
     output_file.write(verilog_code)
 
